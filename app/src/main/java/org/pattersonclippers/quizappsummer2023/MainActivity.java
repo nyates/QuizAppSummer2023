9c326fafe5a69caa5c9898a50a7dde01a2c25997
Yates <nyates@bcps.k12.md.us>
2023-07-24 15:41:20 -0400

    added my first question to the quiz and a toast to say right/wrong

--- app/src/main/java/org/pattersonclippers/quizappsummer2023/MainActivity.java
+++ app/src/main/java/org/pattersonclippers/quizappsummer2023/MainActivity.java
@@ -3,12 +3,46 @@ package org.pattersonclippers.quizappsummer2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView questionTV;
+    Button falseBTN, trueBTN;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        questionTV = (TextView) findViewById(R.id.questionTV);
+        falseBTN = (Button) findViewById(R.id.falseBTN);
+        trueBTN = (Button) findViewById(R.id.trueBTN);
+
+        falseBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = "You are correct! Good job!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(getApplicationContext(), message, duration);
+                toast.show();
+            }
+        });
+
+        falseBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = "You are wrong! Try again!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(getApplicationContext(), message, duration);
+                toast.show();
+            }
+        });
     }
 }
